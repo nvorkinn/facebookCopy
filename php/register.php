fd62fcff542979f9bd157881a9bc401c7517a4a1
Nikolai Vorkinn <n.vorkinn@gmail.com>
2014-02-01 15:27:55 +0000

    refactoring to using php-includes file

--- php/register.php
+++ php/register.php
@@ -1,20 +1,10 @@
 <?php
+include '../includes/php-includes.php';
 
-// Create connection
-$connection = mysqli_connect("localhost","root","root", "facebookcopy");
+$privacy_setting_insert = "INSERT INTO privacy_setting (visible_to, name) VALUES (99, 'somename')";
 
-// Check connection
-if (mysqli_connect_errno()) {
-	
-	echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
-
-$sql = "INSERT INTO privacy_setting (visible_to, name)
-		VALUES
-		(99, 'somename')";
-
-if (!mysqli_query($connection, $sql)) {
-	
+if (!mysqli_query($connection, $privacy_setting_insert)) {
+	echo "Could not insert entry into the privacy_setting table";
 	die('Error: ' . mysqli_error($connection));
 }
 echo "1 record added in privacy_setting \n";
@@ -27,19 +17,9 @@ $lastname = mysqli_real_escape_string($connection, $_POST["lastname"]);
 $birthdate = mysqli_real_escape_string($connection, $_POST["birthdate"]);
 $password = rand(1000, 5000);
 
-$sql2 = "INSERT INTO profile (type, privacy_setting_id, photo_code, username,
-		name, surname, dob, email, password)
-		VALUES
-		(99, $privacy_id, 99,
-		'". $email ."',
-		'". $firstname ."',
-		'". $lastname ."',
-		'". $birthdate ."',
-		'". $email ."',
-		'". md5($password) ."') ";
-
-if (!mysqli_query($connection ,$sql2)) {
-
+$profile_insert = "INSERT INTO profile (type, privacy_setting_id, photo_code, username, name, surname, dob, email, password) VALUES (99, $privacy_id, 99, '". $email ."', '". $firstname ."', '". $lastname ."', '". $birthdate ."', '". $email ."', '". md5($password) ."') ";
+if (!mysqli_query($connection ,$profile_insert)) {
+	echo "Could not insert entry into the profile table";
 	die('Error: ' . mysqli_error($connection));
 }
 echo "1 record added in profile";
@@ -47,20 +27,11 @@ echo "1 record added in profile";
 $profile_id = mysqli_insert_id($connection);
 $hash = md5(rand(0, 1000));
 
-mysqli_query($connection,
-"INSERT INTO user (profile_id, admin, verified, online, hash)
-VALUES (
-$profile_id,
-99,
-0,
-99,
-'". $hash ."')") or die(mysqli_error());
-
-mail(
-'n.vorkinn@gmail.com', // your email address
-'Test', // email subject
-'This is an email' // email body
-);
+$user_insert = "INSERT INTO user (profile_id, admin, verified, online, hash) VALUES (".$profile_id.", 99, 0, 99, '". $hash ."')";
+if (!mysqli_query($connection, $user_insert)) {
+	echo "Could not insert entry into user table";
+	die('Error: '.mysqli_error());
+}
 
 $subject = 'FacebookCopy | Profile Verification';
 $message = '
