10ec069a2c958a05bb0ec8cc6c7ec1dd18bbec4b
Nikolai Vorkinn <n.vorkinn@gmail.com>
2014-02-02 10:51:33 +0000

    indentation

--- php/register.php
+++ php/register.php
@@ -1,60 +1,59 @@
-<?php
-require ('../includes/php-includes.php');
+<?PHP
+	require ('../includes/php-includes.php');
 
-// Adding privacy_setting entry. This is required to make a profile
-$privacy_setting_insert = "INSERT INTO privacy_setting (visible_to, name) VALUES (99, 'somename')";
+	// Adding privacy_setting entry. This is required to make a profile
+	$privacy_setting_insert = "INSERT INTO privacy_setting (visible_to, name) VALUES (99, 'somename')";
 
-if (!mysqli_query($link, $privacy_setting_insert)) {
-	echo "Could not insert entry into the privacy_setting table";
-	die('Error: ' . mysqli_error($link));
-}
+	if (!mysqli_query($link, $privacy_setting_insert)) {
+		echo "Could not insert entry into the privacy_setting table";
+		die('Error: ' . mysqli_error($link));
+	}
 
-// Get the auto-incremented value
-$privacy_id = mysqli_insert_id($link);
+	// Get the auto-incremented value
+	$privacy_id = mysqli_insert_id($link);
 
-// Get and escape values of web page fields
-$email = mysqli_real_escape_string($link, $_POST["email"]);
-$firstname = mysqli_real_escape_string($link, $_POST["firstname"]);
-$lastname = mysqli_real_escape_string($link, $_POST["lastname"]);
-$birthdate = mysqli_real_escape_string($link, $_POST["birthdate"]);
-$password = rand(1000, 5000);
+	// Get and escape values of web page fields
+	$email = mysqli_real_escape_string($link, $_POST["email"]);
+	$firstname = mysqli_real_escape_string($link, $_POST["firstname"]);
+	$lastname = mysqli_real_escape_string($link, $_POST["lastname"]);
+	$birthdate = mysqli_real_escape_string($link, $_POST["birthdate"]);
+	$password = rand(1000, 5000);
 
-// Use fields to create a new entry in the profile table
-$profile_insert = "INSERT INTO profile (type, privacy_setting_id, photo_code, username, name, surname, dob, email, password) VALUES (99, $privacy_id, 99, '". $email ."', '". $firstname ."', '". $lastname ."', '". $birthdate ."', '". $email ."', '". md5($password) ."') ";
-if (!mysqli_query($link ,$profile_insert)) {
-	echo "Could not insert entry into the profile table";
-	die('Error: ' . mysqli_error($link));
-}
+	// Use fields to create a new entry in the profile table
+	$profile_insert = "INSERT INTO profile (type, privacy_setting_id, photo_code, username, name, surname, dob, email, password) VALUES (99, $privacy_id, 99, '". $email ."', '". $firstname ."', '". $lastname ."', '". $birthdate ."', '". $email ."', '". md5($password) ."') ";
+	if (!mysqli_query($link ,$profile_insert)) {
+		echo "Could not insert entry into the profile table";
+		die('Error: ' . mysqli_error($link));
+	}
 
-// Get the auto-incremented value from the profile entry and create a random hash
-$profile_id = mysqli_insert_id($link);
-$hash = md5(rand(0, 1000));
+	// Get the auto-incremented value from the profile entry and create a random hash
+	$profile_id = mysqli_insert_id($link);
+	$hash = md5(rand(0, 1000));
 
-// Add a new user table entry with the hash and the profile_id
-$user_insert = "INSERT INTO user (profile_id, admin, verified, online, hash) VALUES (".$profile_id.", 99, 0, 99, '". $hash ."')";
-if (!mysqli_query($link, $user_insert)) {
-	echo "Could not insert entry into user table";
-	die('Error: '.mysqli_error());
-}
+	// Add a new user table entry with the hash and the profile_id
+	$user_insert = "INSERT INTO user (profile_id, admin, verified, online, hash) VALUES (".$profile_id.", 99, 0, 99, '". $hash ."')";
+	if (!mysqli_query($link, $user_insert)) {
+		echo "Could not insert entry into user table";
+		die('Error: '.mysqli_error());
+	}
 
-// Send email verification message to user with hash
-$subject = 'FacebookCopy | Profile Verification';
-$message = '
-Thanks for signing up to FacebookCopy!
-A user account has been created for you. The cridentials you need to log in are listed below:
+	// Send email verification message to user with hash
+	$subject = 'FacebookCopy | Profile Verification';
+	$message = '
+	Thanks for signing up to FacebookCopy!
+	A user account has been created for you. The cridentials you need to log in are listed below:
 
-*************************************
-Username: '.$email.'
-Password: '.$password.'
-*************************************
+	*************************************
+	Username: '.$email.'
+	Password: '.$password.'
+	*************************************
 			
-Please click the link below to activate the account:
+	Please click the link below to activate the account:
 
-http://localhost/php/validate_user.php?email='.$email.'&hash='.$hash.'
+	http://localhost/php/validate_user.php?email='.$email.'&hash='.$hash.'
 			
-';
+	';
 
-$headers = 'From: Me <nikolai.vorkinn.10@ucl.ac.uk>';
+	$headers = 'From: Me <nikolai.vorkinn.10@ucl.ac.uk>';
 
-mail($email, $subject, $message, $headers);
-?>
+	mail($email, $subject, $message, $headers);
--- php/validate_user.php
+++ php/validate_user.php
@@ -1,33 +1,33 @@
-<?php
-require ('../includes/php-includes.php');
+<?PHP
+	require ('../includes/php-includes.php');
 
-// Ensure both email and hash are present in URL
-if(isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && !empty($_GET['hash'])){
+	// Ensure both email and hash are present in URL
+	if(isset($_GET['email']) && !empty($_GET['email']) AND isset($_GET['hash']) && !empty($_GET['hash'])){
 	
-	// Get data off URL
-	$email = mysqli_escape_string($link, $_GET['email']);
-	$hash = mysqli_escape_string($link, $_GET['hash']);
+		// Get data off URL
+		$email = mysqli_escape_string($link, $_GET['email']);
+		$hash = mysqli_escape_string($link, $_GET['hash']);
 	
-	// Search for un-verified entry
-	$search = mysqli_query($link, "SELECT email, hash, verified FROM user u, profile p
-				WHERE email='".$email."'AND hash='".$hash."' AND verified=0
-				AND p.id = u.profile_id") or die(mysql_error($link));
-	$match = mysqli_num_rows($search);
+		// Search for un-verified entry
+		$search = mysqli_query($link, "SELECT email, hash, verified FROM user u, profile p
+					WHERE email='".$email."'AND hash='".$hash."' AND verified=0
+					AND p.id = u.profile_id") or die(mysql_error($link));
+		$match = mysqli_num_rows($search);
 	
-	// If there was a match, set it to active
-	if ($match > 0) {
+		// If there was a match, set it to active
+		if ($match > 0) {
 		
-		mysqli_query($link, "UPDATE user u, profile p SET verified=1
-		WHERE email='".$email."' AND hash='".$hash."'AND verified=0
-		AND p.id = u.profile_id") or die(mysqli_error($link));
+			mysqli_query($link, "UPDATE user u, profile p SET verified=1
+			WHERE email='".$email."' AND hash='".$hash."'AND verified=0
+			AND p.id = u.profile_id") or die(mysqli_error($link));
+		}
+		else {
+		
+			echo 'There were no results in the database with that email and hash,
+					or it has already been verified.';
+		}
 	}
 	else {
-		
-		echo 'There were no results in the database with that email and hash,
-				or it has already been verified.';
-	}
-}
-else {
 	
-	echo "Either email or hash isn't set properly in URL";
-}
+		echo "Either email or hash isn't set properly in URL";
+	}
