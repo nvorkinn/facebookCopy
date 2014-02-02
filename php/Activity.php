51fd4170684812280bc9fb494050c82d9485d3c3
Jay Nanavati <jaysnanavati@hotmail.co.uk>
2014-02-02 23:34:41 +0000

    refactoring

--- php/Activity.php
+++ php/Activity.php
@@ -8,31 +8,33 @@ class Activity
 	var $to_user_id;
 	var $type;
 	var $created_date;
+	var $mysqli;
 	
 	function __construct($from_user_id=NULL, $to_user_id=NULL,$type=NULL) 
 	{
+	global $mysqli;
 	$this->from_user_id=$from_user_id;
 	$this->to_user_id=$to_user_id;
 	$this->type=$type;
+	$this->mysqli=$mysqli;
 	}
 	
 	function save(){
 		$this->created_date = date("Y-m-d H:i:s");
-		global $mysqli;
-		
+
 		if($this->type==0){
-			$this->makeFriendRequest($mysqli);
+			$this->makeFriendRequest();
 		}	
 	}
 	
-	private function makeFriendRequest($mysqli){
+	private function makeFriendRequest(){
 		$if_friend_request ="SELECT from_user_id,to_user_id,type FROM activity WHERE from_user_id='$this->from_user_id' AND to_user_id='$this->to_user_id' AND type='$this->type'";
 			
-			if ($result = $mysqli->query($if_friend_request)){
+			if ($result = $this->mysqli->query($if_friend_request)){
 				if ($result->num_rows == 0){
 					$query="INSERT INTO activity (from_user_id, to_user_id, type, created_date)VALUES ('$this->from_user_id', '$this->to_user_id','$this->type','$this->created_date')";
-					$mysqli->query($query);
-					$this->id=$mysqli->insert_id;
+					$this->mysqli->query($query);
+					$this->id=$this->mysqli->insert_id;
 				}
 			}
 	}
--- php/Notification.php
+++ php/Notification.php
@@ -5,25 +5,27 @@ class Notification
 	var $activity_id;
 	var $target_id;
 	var $created_date;
+	var $mysqli;
 	
 	function __construct($activity_id=NULL, $target_id=NULL) 
 	{
+	global $mysqli;
 	$this->activity_id=$activity_id;
 	$this->target_id=$target_id;
+	$this->mysqli=$mysqli;
 	}
 	
 	function save(){
-	global $mysqli;
 	$this->created_date = date("Y-m-d H:i:s");
 	
 	$query="INSERT INTO notification (activity_id, target_id, seen, created_date)VALUES ('$this->activity_id', '$this->target_id',0, '$this->created_date')";
 	
-	if (!$result = $mysqli->query($query)){
+	if (!$result = $this->mysqli->query($query)){
 		echo "DB Error, could not query the database\n";
 		echo 'MySQL Error: ' . mysql_error();
 		exit;
 	}
-	$this->id=$mysqli->insert_id;
+	$this->id=$this->mysqli->insert_id;
 	}
 	
 	function notify(){
