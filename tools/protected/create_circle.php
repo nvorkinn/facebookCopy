29790094fdd5ad440ac4d2e35659afb5c0a06427
Nikolai Vorkinn <n.vorkinn@gmail.com>
2014-02-14 13:53:16 +0000

    revert var_dump

--- tools/protected/create_circle.php
+++ tools/protected/create_circle.php
@@ -2,14 +2,11 @@
 	require($_SERVER["DOCUMENT_ROOT"] . "/includes/php-includes.php");
 	
 	$owner_id = $_SESSION["user_id"];
-	//$circle_name = $_POST["circle_name"];
-	error_log(var_dump($_POST));
+	$circle_name = $_POST["circle_name"];
 	
 	$query = "INSERT INTO circle (user_owner_id, name) VALUES ($owner_id, '$circle_name')";
 	
 	if (!$mysqli->query($query)) {
-		echo "DB Error, could not create new circle in database\n";
-		echo "MySQL Error: " . mysql_error();
-		exit;	
+		echo -1;	
 	}
 ?>
